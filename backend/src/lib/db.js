410317ef25fabffcb2238f22f4806eaad5735e0c
KevinAbraham07 <kevinrajish2005@gmail.com>
2025-11-06 00:08:56 +0530

    initial commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log("MongoDB connection error:", error);
+    const connect = await mongoose.connect(`${process.env.MONGO_URI}`);
+    if (connect) {
+      console.log("Mongo Connected");
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
